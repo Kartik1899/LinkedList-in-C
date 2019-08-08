289d38aad6519ff561330a1f6d02ae4878e30410
Kartik1899 <ranakartik16@gmail.com>
2019-08-08 15:42:04 +0530

    Structure of List

---
+++ LinkedListStructure.c
@@ -0,0 +1,6 @@
+typedef struct linkedlist {
+    int value;                          // stores the value of the node;
+    
+    struct linkedlist *address;;        // stores the address of the next node;
+
+}LinkedList;                            // Naming the structure
